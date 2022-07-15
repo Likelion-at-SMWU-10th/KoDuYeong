2f0c6d955cb773885680290fec87179f8d1596cf
duyeong-ko <329_143@naver.com>
2022-07-15 18:27:07 +0900

    10주차 과제

---
+++ week10_assign/routeassign/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      {/* 개인페이지 제목 */}
+      <h1>시원한 망고빙수가 먹고싶어요</h1>
+
+      {/* 고두영의 개인페이지입니다. */}
+      <p>고두영의 개인페이지입니다.</p>
+    </div>
+  )
+};
+
+export default Home;
